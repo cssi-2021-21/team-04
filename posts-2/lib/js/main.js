0d66f84c45d60efc95fa08fe3d6c0c29112adcfe
ywu3760 <ywu3760@bths.edu>
2021-08-04 14:21:52 -0400

    fixed undefined values being passed

---
+++ posts-2/lib/js/main.js
@@ -0,0 +1,25 @@
+console.log("main.js running.");
+
+// APP.registerListenerTarget("test", callback => {
+//     window.setInterval(() => {
+//         const val = Math.random()*100;
+//         console.log("new value", val);
+//         callback(val);
+//     }, 2000);
+// });
+
+// APP.registerListener("test", (id, val) => {
+//     console.log("listener", id, val);
+//     // APP.killListener(id);
+// });
+
+// window.setTimeout(() => {
+//     APP.registerListener("test", (id, val) => {
+//         console.log("listener", id, val);
+//         // APP.killListener(id);
+//     }, false);
+// }, 4000)
+
+APP.registerListener(DEFAULT_TARGETS.userData, (_, user) => {
+    console.log(user);
+}, false);
