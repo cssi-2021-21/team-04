bba24f79e3944ce3c6f47f0dfadd1d5580b4453a
YAttiax <attia_youssef@hotmail.com>
2021-08-04 13:42:34 -0400

    reading and writing using APP.js

--- profile/lib/js/main.js
+++ profile/lib/js/main.js
@@ -1,15 +1,5 @@
 console.log("script running.");
 
-// # FIREBASE
-firebase.initializeApp({
-    apiKey: "AIzaSyBJxja4Sd-os7iEiIOHpwtj0VgDtmUUO-0",
-    authDomain: "cssi-2021-team-04.firebaseapp.com",
-    projectId: "cssi-2021-team-04",
-    storageBucket: "cssi-2021-team-04.appspot.com",
-    messagingSenderId: "572149620254",
-    appId: "1:572149620254:web:e4f78ac7128d8d713cd80f"
-});
-
 const activities = {
     "walk":4,
     "run":11,
@@ -202,3 +192,4 @@ const hideWorkoutModal = () => {
     workoutModal.style.display = "none"
 }
 //#endregion
+
